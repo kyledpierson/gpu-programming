e363fab7b1c17dbfba32845dc3d7875d276731fc
Kyle Pierson <u0632972@kingspeak2.wasatch.peaks>
2018-03-17 16:46:21 -0600

    Added project

---
+++ project/runscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=soc-gpu-kp
+#SBATCH --partition=soc-gpu-kp
+#SBATCH --job-name=scattering
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:p100:1
+#SBATCH --mem=10G
+#SBATCH --time=00:10:00
+#SBATCH --export=ALL
+#SBATCH --qos=soc-gpu-kp
+#SBATCH -o scattering.out
+#SBATCH -e scattering.err
+
+ulimit -c unlimited -s
+
+./scattering
+# nvprof --analysis-metrics -o analysis.nvvp ./scattering
+
